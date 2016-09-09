8efb00dbbc352ceff5a907563bb91351b4688211
Ravikiran <raviramesh.kiran97@gmail.com>
2016-09-09 18:32:53 +0530

    final game v1

---
+++ festember_game/android/src/com/festember/festember16/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.festember.festember16;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.festember.festember16.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(new NativeCode(this)), config);
+
+
+	}
+}
